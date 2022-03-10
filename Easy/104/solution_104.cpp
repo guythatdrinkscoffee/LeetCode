656ab8a1114fb2ed1829ebdbef87bafdbe15e3bd
J.Manuel Zaragoza <zaragozajn@gmail.com>
2022-03-09 19:25:48 -0800

    Solution to problem 104

---
+++ Easy/104/solution_104.cpp
@@ -0,0 +1,33 @@
+int maxDepth(TreeNode* root) {
+    if(!root){
+        return 0;
+    }
+    
+    int depth = 0;
+    queue<TreeNode*> q;
+    q.push(root);
+    
+    while(!q.empty()){
+        int count = q.size();
+        depth++;
+        
+        while(count > 0){
+            TreeNode *current = q.front();
+            q.pop();
+            
+            if(current->left){
+                q.push(current->left);
+            }
+            
+            if(current->right){
+                q.push(current->right);
+            }
+            
+            count--;
+        }
+        
+    
+    }
+    
+    return depth;
+}
