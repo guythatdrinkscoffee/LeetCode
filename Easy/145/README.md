25daf2dc91f335204be7381a330903cbdbb66ec1
J.Manuel Zaragoza <zaragozajn@gmail.com>
2022-03-11 10:30:04 -0800

    Solution to problem 145

---
+++ Easy/145/README.md
@@ -0,0 +1,14 @@
+# 145. Binary Tree Postorder Traversal
+
+# Problem Description
+
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+# Test Cases
+
+- Input: root = [1,null,2,3]
+- Output: [3,2,1]
+
+## Solution
+
+Pattern used: Depth-First Search
---
+++ Easy/145/solution_145.cpp
@@ -0,0 +1,16 @@
+vector<int> postorderTraversal(TreeNode* root) {
+    vector<int> ans;
+    postorder(root, ans);
+    
+    return ans;
+}
+
+void postorder(TreeNode *root, vector<int> &ans){
+    if(!root){
+        return;
+    }
+    
+    postorder(root->left, ans);
+    postorder(root->right, ans);
+    ans.push_back(root->val);
+}
