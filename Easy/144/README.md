1b7c1bff678d20a02472cbd975e119f698470a7f
J.Manuel Zaragoza <zaragozajn@gmail.com>
2022-03-11 10:24:40 -0800

    Solution to problem 144

---
+++ Easy/144/README.md
@@ -0,0 +1,14 @@
+# 144. Binary Tree Preorder Traversal
+
+# Problem Description
+
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+# Test Cases
+
+- Input: root = [1,null,2,3]
+- Output: [1,2,3]
+
+## Solution
+
+Pattern used: Depth-First Search
---
+++ Easy/144/solution_144.cpp
@@ -0,0 +1,16 @@
+vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> ans; 
+    preorder(root, ans);
+    
+    return ans;
+}
+
+void preorder(TreeNode* root, vector<int> &ans){
+    if(!root){
+        return; 
+    }
+    
+    ans.push_back(root->val);
+    preorder(root->left, ans);
+    preorder(root->right, ans);
+}
